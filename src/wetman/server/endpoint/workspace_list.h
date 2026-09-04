b4d8352f457bfdde005a339dd98195a442fe2e14
Mikhail Komarov <komarov.ma@phystech.edu>
2026-09-04 09:06:07 +0200

    Implemented workspace_list endpoint

---
+++ src/wetman/server/endpoint/workspace_list.h
@@ -0,0 +1,9 @@
+#ifndef WETMAN_SERVER_ENDPOINT_WORKSPACE_LIST_H
+#define WETMAN_SERVER_ENDPOINT_WORKSPACE_LIST_H
+
+#include <wetman/shared/endpoint/workspace_list.h>
+#include <wetman/utils/net/macro.h>
+
+ENDPOINT_DECLARE_SERVER(Endpoint_WorkspaceList)
+
+#endif // WETMAN_SERVER_ENDPOINT_WORKSPACE_LIST_H
