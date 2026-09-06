8d942a348ff42fcea3b20e8a891c2180017cb7c7
Mikhail Komarov <komarov.ma@phystech.edu>
2026-09-06 22:21:55 +0200

    Separate server instance creation/shutdown from integration test cases

--- test/integration/it_healthcheck.c
+++ test/integration/it_healthcheck.c
@@ -13,24 +13,10 @@ TEST(IntegrationTest_HealthCheck_NoServerRunning)
 
 TEST(IntegrationTest_HealthCheck_Success)
 {
-    char wdir[256];
-    CREATE_TMP_DIR(wdir);
-
-    char serverCmd[512];
-    snprintf(serverCmd, sizeof(serverCmd),
-            "WETMAN_WDIR=%s ../build/wetman_server", wdir);
-
-    pid_t serverPid = Subprocess_RunCommand(serverCmd);
-    ASSERT_NE(serverPid, -1);
-
-    sleep(1);
-
     pid_t clientPid = Subprocess_RunCommand("../build/wetman healthcheck");
     ASSERT_NE(clientPid, -1);
 
     i32 statusCode = Subprocess_WaitFor(clientPid, 5000);
     EXPECT_EQ(statusCode, 0);
-
-    ASSERT(Subprocess_Kill(serverPid));
 }
 
--- test/integration/mod.c
+++ test/integration/mod.c
@@ -2,8 +2,46 @@
 
 #include "it_healthcheck.c"
 
+// Tmp dir macros work only in the context of the test case,
+// so we need to wrap it into a separate test
+static pid_t __serverPid;
+
+TEST(IntegrationTest_RunServer)
+{
+    char wdir[256];
+    CREATE_TMP_DIR(wdir);
+
+    char serverCmd[512];
+    snprintf(serverCmd, sizeof(serverCmd),
+            "WETMAN_WDIR=%s ../build/wetman_server", wdir);
+
+    __serverPid = Subprocess_RunCommand(serverCmd);
+    ASSERT_NE(__serverPid, -1);
+
+    sleep(1);
+
+    // Check that second server instance cannot be run
+    pid_t secondServerPid = Subprocess_RunCommand(serverCmd);
+    ASSERT_NE(__serverPid, -1);
+
+    i32 statusCode = Subprocess_WaitFor(secondServerPid, 5000);
+    ASSERT_EQ(statusCode, 1);
+}
+
+TEST(IntegrationTest_ShutDownServer)
+{
+    ASSERT(Subprocess_Kill(__serverPid));
+}
+
 void registerIntegrationTests(void)
 {
+    // Tests without running server
     REGISTER_TEST(IntegrationTest_HealthCheck_NoServerRunning);
+
+    // Tests required server instance running
+    REGISTER_TEST(IntegrationTest_RunServer);
+
     REGISTER_TEST(IntegrationTest_HealthCheck_Success);
+
+    REGISTER_TEST(IntegrationTest_ShutDownServer);
 }
