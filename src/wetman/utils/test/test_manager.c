fc53a60ecca63c50414aac1a65a3a7adde43c366
Mikhail Komarov <komarov.ma@phystech.edu>
2026-09-06 18:50:21 +0200

    Fixed build scripts: added flag to declare POSIX symbols

--- src/wetman/utils/test/test_manager.c
+++ src/wetman/utils/test/test_manager.c
@@ -3,12 +3,13 @@
 #include <wetman/utils/filesystem.h>
 #include <wetman/utils/time.h>
 
+#include <errno.h>
 #include <fcntl.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
-#include <errno.h>
 
 const char* __resultLabel(i32 success)
 {
