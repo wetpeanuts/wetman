99d7ad64c4842177d2eddd4012225e521f3af5e7
Mikhail Komarov <komarov.ma@phystech.edu>
2026-09-06 19:02:25 +0200

    Ported file lock to universal UNIX API

--- src/wetman/shared/persistence/workspace_config.c
+++ src/wetman/shared/persistence/workspace_config.c
@@ -4,6 +4,7 @@
 #include <wetman/utils/filesystem.h>
 
 #include <fcntl.h>
+#include <sys/file.h>
 #include <unistd.h>
 
 
@@ -14,11 +15,12 @@ WorkspaceConfig WorkspaceConfig_Read(
 {
     WorkspaceConfig config = {0};
 
-    i32 fd = FS_OpenFile(filePath, O_RDONLY | O_EXLOCK);
+    i32 fd = FS_OpenFile(filePath, O_RDONLY);
     if (fd == -1) {
         *status = PERSISTENCE_STATUS_FILE_DOES_NOT_EXIST;
         return config;
     }
+    flock(fd, LOCK_SH);
     // TODO: Allow read without setting max len
     DataStream dataStream = DataStream_Read(fd, arena, 4096);
 
@@ -42,10 +44,11 @@ PersistenceStatus WorkspaceConfig_Write(
         Str                    filePath,
         const WorkspaceConfig* config)
 {
-    i32 fd = FS_OpenFile(filePath, O_WRONLY | O_TRUNC | O_EXLOCK);
+    i32 fd = FS_OpenFile(filePath, O_WRONLY | O_TRUNC);
     if (fd == -1) {
         return PERSISTENCE_STATUS_FILE_DOES_NOT_EXIST;
     }
+    flock(fd, LOCK_EX);
 
     Arena arena = Arena_New();
     DataStream dataStream = DataStream_New();
