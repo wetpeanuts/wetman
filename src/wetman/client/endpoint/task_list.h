43830e840ce9ec63297a321ae5a37e528598e29b
Mikhail Komarov <komarov.ma@phystech.edu>
2026-09-18 13:55:35 +0200

    Implemented task_list CLI command
    Closes #17

---
+++ src/wetman/client/endpoint/task_list.h
@@ -0,0 +1,9 @@
+#ifndef WETMAN_CLIENT_ENDPOINT_TASK_LIST_H
+#define WETMAN_CLIENT_ENDPOINT_TASK_LIST_H
+
+#include <wetman/shared/endpoint/task_list.h>
+#include <wetman/utils/net/macro.h>
+
+ENDPOINT_DECLARE_CLIENT(Endpoint_TaskList)
+
+#endif // WETMAN_CLIENT_ENDPOINT_TASK_LIST_H
